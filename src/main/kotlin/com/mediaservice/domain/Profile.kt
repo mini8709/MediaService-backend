c2b6ae6d87a3fe38edaf95eec5077b97f921343c
KwangpyoKo <81270740+KwangpyoKo@users.noreply.github.com>
2021-12-03 17:28:23 +0900

    Fix attribute user_id in Profile Table (#29)
    * feat:Implement profile findById && exception model
    
    * feat:Implement DataNotFoundException
    
    * feat:Implement DataNotFoundException
    
    * fix: Fix column user_id in Profile
    
    * refactor: Refactor code convention in Profile
    
    * refactor: Refactor import module
    
    Co-authored-by: 고광표 <kwangpyo@gogwangpyoui-MacBook-Pro.local>

--- src/main/kotlin/com/mediaservice/domain/Profile.kt
+++ src/main/kotlin/com/mediaservice/domain/Profile.kt
@@ -5,16 +5,16 @@ import org.jetbrains.exposed.dao.UUIDEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.dao.id.UUIDTable
 import org.jetbrains.exposed.sql.Column
-import java.util.*
+import java.util.UUID
 
-object ProfileTable: UUIDTable("TB_PROFILE"){
-    val user = reference("user", UserTable)
+object ProfileTable: UUIDTable("TB_PROFILE") {
+    val user_id = reference("user_id", UserTable)
     val name: Column<String> = varchar("name", 255)
     val rate: Column<String> = varchar("age_rate", 255)
     val mainImage: Column<String> = varchar("main_image", 255)
 }
 
-class Profile(var id: UUID, var user: User, var name: String, var rate: String, var mainImage: String){
+class Profile(var id: UUID, var user: User, var name: String, var rate: String, var mainImage: String) {
     companion object {
         fun from(profileEntity: ProfileEntity) = Profile(
                 id = profileEntity.id.value,
@@ -26,9 +26,9 @@ class Profile(var id: UUID, var user: User, var name: String, var rate: String, 
     }
 }
 
-class ProfileEntity(id: EntityID<UUID>): UUIDEntity(id){
+class ProfileEntity(id: EntityID<UUID>): UUIDEntity(id) {
     companion object: UUIDEntityClass<ProfileEntity>(ProfileTable)
-    var user by UserEntity referencedOn ProfileTable.user
+    var user by UserEntity referencedOn ProfileTable.user_id
     var name by ProfileTable.name
     var rate by ProfileTable.rate
     var mainImage by ProfileTable.mainImage
