221f7fa95e160afe43c6d630dbdd11c7aae0487c
Minsoo <37828653+als95@users.noreply.github.com>
2021-12-08 17:47:59 +0900

    fix: Fix exposed ddl create to drop and create (#31)

--- src/main/kotlin/com/mediaservice/MediaServiceBackendApplication.kt
+++ src/main/kotlin/com/mediaservice/MediaServiceBackendApplication.kt
@@ -1,5 +1,9 @@
 package com.mediaservice
 
+import com.mediaservice.domain.ProfileTable
+import com.mediaservice.domain.UserTable
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.transactions.transaction
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -16,4 +20,11 @@ class MediaServiceBackendApplication
 @NoCoverage
 fun main(args: Array<String>) {
     runApplication<MediaServiceBackendApplication>(*args)
+
+    if (System.getProperty("spring.profiles.active") == "local") {
+        transaction {
+            SchemaUtils.drop(UserTable, ProfileTable)
+            SchemaUtils.create(UserTable, ProfileTable)
+        }
+    }
 }
