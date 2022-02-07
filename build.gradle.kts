e083d83983cbb52c25d7ef217c7a7dd93d093d84
ChanghyukWoo <89950941+ChanghyukWoo@users.noreply.github.com>
2022-02-07 20:47:23 +0900

    feat: redis connection (#109)
    Co-authored-by: changhyuk woo <changhyuk_woo@tmax.co.kr>

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
     implementation("io.jsonwebtoken:jjwt-api:0.11.2")
     implementation("io.jsonwebtoken:jjwt-jackson:0.11.2")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.2")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
 
     testImplementation("org.springframework.security:spring-security-test")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
